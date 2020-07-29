b727ced5ee3b93b3645f36fba6f507f184fe8db5
victor.soares <victor.soares@solucionarelj.com.br>
2020-07-29 18:39:12 -0300

    Criação do formulário do cadastro de categorias

--- src/pages/cadastro/categoria/index.js
+++ src/pages/cadastro/categoria/index.js
@@ -1,20 +1,71 @@
-import React from "react";
-import PageDefault from "../../../components/pageDefault";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import PageDefault from "../../../components/pageDefault";
+import FormField from "../../../components/formField";
 
 function CadastroCategoria() {
+  const valoresIniciais = {
+    name: "",
+    description: "",
+    color: "",
+  };
+  const [categorias, setCategorias] = useState([]);
+  const [values, setValues] = useState(valoresIniciais);
+
+  function setValue(key, value) {
+    setValues({
+      ...values,
+      [key]: value,
+    });
+  }
+
+  function handleChange(info) {
+    setValue(info.target.getAttribute("name"), info.target.value);
+  }
+
   return (
     <PageDefault>
-      <h1>Cadastro de Categoria</h1>
+      <h1>Cadastro de Categoria: {values.name}</h1>
 
-      <form>
-        <label>
-          Nome da Categoria:
-          <input type="text" />
-        </label>
+      <form
+        onSubmit={function handleSubmit(info) {
+          info.preventDefault();
+          setCategorias([...categorias, values]);
+          setValues(valoresIniciais);
+        }}
+      >
+        <FormField
+          label="Nome da Categoria"
+          type="text"
+          name="name"
+          value={values.name}
+          onChange={handleChange}
+        />
+
+        <FormField
+          label="Descrição"
+          type="text"
+          name="description"
+          value={values.description}
+          onChange={handleChange}
+        />
+
+        <FormField
+          label="Cor"
+          type="color"
+          name="color"
+          value={values.color}
+          onChange={handleChange}
+        />
 
         <button>Cadastrar</button>
       </form>
+
+      <ul>
+        {categorias.map((categoria, indice) => {
+          return <li key={`${categoria}${indice}`}>{categoria.name}</li>;
+        })}
+      </ul>
 
       <Link to="/">Ir para home</Link>
     </PageDefault>
